30cc257c3b2625bd6e078ee51fbca04ad74673a2
Vladimir Tsvang <vtsvang@gmail.com>
2011-07-24 17:18:24 +0400

    Simple speed test added

--- tests/tests.js
+++ tests/tests.js
@@ -6,6 +6,17 @@
       deepEqual($().jCache('.test-data div').get(), $('.test-data div').get(), 'Deep');
       deepEqual($().jCache('div', '#div').get(), $('div', '#div').get(), 'Element name & context');
       deepEqual($().jCache('div', '#div, #div2').get(), $('div', '#div, #div2').get(), 'If context is multitude');
+      
+      console.time('Average speed with cache');
+        for(var i = 0; i < 1000; i++) {
+          $().jCache('div');
+        }
+      console.timeEnd('Average speed with cache');
+      console.time('Average speed without cache');
+        for(var j = 0; j < 1000; j++) {
+          $('div');
+        }
+      console.timeEnd('Average speed without cache');
 
     });
   });
