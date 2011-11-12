191f6d072e8da874c8aee0b0c573355b36af7fa8
Vladimir Tsvang <vtsvang@gmail.com>
2011-11-12 16:19:40 +0400

    Source has been refactored

--- jCache.js
+++ jCache.js
@@ -14,70 +14,86 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see http://www.gnu.org/licenses/.
  */
-(function($, undefined) {  
-  var 
-  jCache = function(selector, context) {
-    var 
-      se     = selector || false,
-      co     = context  || false;
-    if(selector) {
-      return jCache.get(se, co);
-    }
-    return jCache;
-  };
-
-  jCache['clear'] = function(selector, context) {
-    if(selector == undefined) {
-      jCache.objectsCollection = new Object();
-      return;
-    }
-    if(context) {
-      if(jCache.objectsCollection[context] !== undefined) {
-        if(jCache.objectsCollection[context][selector] !== undefined) {
-          jCache.objectsCollection[context][selector] = undefined;
-        }
-      }
-    } else {
-      jCache.objectsCollection[selector] = undefined;
-    }
-  };
-
-  jCache['get'] = function(selector, context) {
-    if(typeof(selector) == 'string') {
-      if(context) {
-        if(context.length == 0) {
-          return $();
-        }
-        var _context = jCache.get(context);
-        if(jCache.objectsCollection[context][selector] === undefined) {
-          jCache.objectsCollection[context][selector] = $(selector, _context);
-        }
-        return jCache.objectsCollection[context][selector];
-      } else {
-        if(jCache.objectsCollection[selector] === undefined) {
-          jCache.objectsCollection[selector] = new Object();
-          if(selector instanceof jQuery) {
-            return selector;
-          }
-          if(jCache.objectsCollection[selector]['_object'] === undefined) {
-            jCache.objectsCollection[selector]['_object'] = $(selector);
-          }
-        }
-        return jCache.objectsCollection[selector]['_object'];
-      }
-    }
-    if(context) {
-      return $(selector, context);
-    } else {
-      if(jCache.objectsCollection[selector] == undefined) {
-        jCache.objectsCollection[selector] = new Object();
-      }
-      return jCache.objectsCollection[selector]['_object'] = $(selector);
-    }
-  };
+(function($, w, undefined) {
     
-  jCache['objectsCollection'] = new Object();
-  
-  window['jCache'] = jCache;
-  
-})(jQuery);
+    /**
+     * Creates instance of jCache
+     * 
+     * @constructor
+     */
+    var jCache = function() {
+	if(this instanceof jCache) {
+	    this.cache = new Object();
+	    
+	    var 
+		proxy = $.proxy(this.get, this);
+	    
+	    proxy.remove = $.proxy(jCache.mixin.remove, this);
+	    proxy.clear = $.proxy(jCache.mixin.clear, this);
+	    
+	    return proxy;
+	}
+	return new jCache();
+    };
+    
+    /**
+     * Converts arguments object to array
+     */
+    jCache.getArgs = function(args) {
+	return Array.prototype.slice.call(args);
+    };
+    
+    jCache.mixin = {
+	/**
+	 * Removes element from cache
+	 * 
+	 * @example
+	 *	$c.remove('div');
+	 */
+	remove : function() {
+	    var args = jCache.getArgs(arguments);
+	    
+	    if(this.cache[args]) {
+		delete this.cache[args];
+	    }
+	    
+	    return this;
+	},
+	
+	/**
+	 * Clear cache
+	 * 
+	 * @example
+	 *	$.clear();
+	 */
+	clear : function() {
+	    this.cache = new Object();
+	    return this;
+	}
+    };
+	
+    
+    jCache.prototype = {
+	
+	get: function() {
+	    var args = jCache.getArgs(arguments);
+	    
+	    if(!this.cache[args]) {
+		this.cache[args] = $.apply(w, args);
+	    }
+	    
+	    return this.cache[args];
+	}
+	
+    };
+    
+    /**
+     * Simple and functional jQuery objects cache
+     * @name $.jCache
+     * @example
+     *	    var $c = new $.jCache();
+     *	    $c('div').fadeToggle(500);
+     */
+    $.jCache = jCache;
+    
+})(jQuery, window);
